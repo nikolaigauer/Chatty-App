5768cca212b6a799690f6b2c1f78a5152f84609f
Nikolai <nikolaigauer@gmail.com>
2017-07-26 01:16:17 +0000

    Add barebone project tree to project, Update json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import React, {Component} from 'react';
+import MessageList from './messageList.jsx';
+import Chatbar from './chatbar.jsx';
 
-class App extends Component {
+
+// let currentUser = {
+//       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+//       messages: [
+//         {
+//           username: "Bob",
+//           content: "Has anyone seen my marbles?",
+//         },
+//         {
+//           username: "Anonymous",
+//           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+//         }
+//       ]
+//     };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+          <nav className="navbar">
+            <a href="/" className="navbar-brand">Chatty</a>
+          </nav>
+          <MessageList/>
+          <Chatbar>
+            <li> {this.state.currentUser.name} </li>
+          </Chatbar>
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<App />, document.getElementById('react-root'), console.log("Rendering <App/>"));
